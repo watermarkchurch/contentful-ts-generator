0ba889a41d3621a67804ebb68bba4a84e3f55941
Gordon <gburgett@watermark.org>
2019-01-27 20:45:11 -0600

    Configure istanbul and add baseline code

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import test from 'ava'
 
+import { sum } from './index'
+
 test('1 + 1 = 2', (t) => {
-  t.true(1 + 1 == 2)
+  t.true(sum(1, 1) == 2)
 })
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+export function sum(a: number, b: number) {
+  return a + b
+}
