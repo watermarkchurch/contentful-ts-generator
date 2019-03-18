865fd55a1ab59c658b5ab82f9b508a87ef879fd6
Gordon <gburgett@watermark.org>
2019-03-17 20:45:29 -0500

    Add a spec proving it works with contentful-js

--- packages/contentful-ts-generator/src/integration.test.ts
+++ packages/contentful-ts-generator/src/integration.test.ts
@@ -1,5 +1,8 @@
 import test, { before } from 'ava'
+import {createClient} from 'contentful'
 import * as fs from 'fs-extra'
+import globby from 'globby'
+import * as nock from 'nock'
 import * as path from 'path'
 import * as tmp from 'tmp'
 import Project from 'ts-morph'
@@ -8,10 +11,15 @@ import { promisify } from 'util'
 import { ContentfulTSGenerator } from './index'
 import { Installer } from './installer'
 
+const client = createClient({
+  accessToken: 'xxx',
+  space: 'testspace',
+  responseLogger,
+  requestLogger,
+} as any)
+
 before(async (t) => {
   const tmpDir = await (promisify<string>((cb) => tmp.dir(cb))())
-  await fs.remove(tmpDir)
-  await fs.mkdirp(tmpDir)
 
   Object.assign(t.context, { tmpDir })
 
@@ -68,7 +76,7 @@ test('Menu can be instantiated from raw entry', async (t) => {
   t.deepEqual(menu.fields.name, 'test name')
 })
 
-test('Menu resolves links', async (t) => {
+test('Menu creates wrapped classes for resolved links', async (t) => {
   const { tmpDir } = (t.context as any)
   const menuPath = path.join(tmpDir, 'generated/menu.ts')
   const buttonPath = path.join(tmpDir, 'generated/menu_button.ts')
@@ -102,3 +110,115 @@ test('Menu resolves links', async (t) => {
   const button = menu.items[0]
   t.true(button instanceof MenuButton)
 })
+
+test('Menu can wrap Contentful.js objects', async (t) => {
+  const { tmpDir } = (t.context as any)
+  const menuPath = path.join(tmpDir, 'generated/menu.ts')
+  const buttonPath = path.join(tmpDir, 'generated/menu_button.ts')
+
+  const { Menu } = require(menuPath)
+  const { MenuButton } = require(buttonPath)
+
+  nockEnvironment()
+  nock('https://cdn.contentful.com')
+    .get('/spaces/testspace/environments/master/entries?sys.id=20bohaVp20MyKSi0YCaw8s')
+    .replyWithFile(200, path.join(__dirname, 'fixtures/menu.json'))
+
+  const menu = new Menu(await client.getEntry<any>('20bohaVp20MyKSi0YCaw8s'))
+  const button = menu.items[0]
+  t.true(button instanceof MenuButton)
+  t.deepEqual(button.text, 'Pricing')
+})
+
+async function loadFixtures() {
+  const fixtures = await Promise.all(
+    (await globby(path.join(__dirname, 'fixtures/*.json')))
+      .map(async (f) => {
+        const fixture = await fs.readFile(f)
+        return [path.basename(f), JSON.parse(fixture.toString())] as [string, any]
+      }),
+    )
+
+  return fixtures.reduce((memo, file: [string, any]) => {
+    memo[file[0]] = file[1]
+    return memo
+  }, {} as { [name: string]: any })
+}
+
+function nockEnvironment() {
+  nock('https://cdn.contentful.com')
+  .get('/spaces/testspace')
+  .reply(200, {
+    name: 'Test Space',
+    sys: {
+      type: 'Space',
+      id: 'testspace',
+      version: 2,
+      createdBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: 'xxxxx',
+        },
+      },
+      createdAt: '2018-01-22T17:49:19Z',
+      updatedBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: 'xxxx',
+        },
+      },
+      updatedAt: '2018-05-30T10:22:40Z',
+    },
+  })
+
+  nock('https://cdn.contentful.com')
+  .get('/spaces/testspace/environments/master')
+  .reply(200, {
+    name: 'master',
+    sys: {
+      type: 'Environment',
+      id: 'master',
+      version: 1,
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: 'testspace',
+        },
+      },
+      status: {
+        sys: {
+          type: 'Link',
+          linkType: 'Status',
+          id: 'ready',
+        },
+      },
+      createdBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: 'xxxx',
+        },
+      },
+      createdAt: '2018-01-22T17:49:19Z',
+      updatedBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: 'xxxx',
+        },
+      },
+      updatedAt: '2018-01-22T17:49:19Z',
+    },
+  })
+}
+
+function requestLogger(config: any) {
+  // console.log('req', config)
+}
+
+function responseLogger(response: any) {
+  console.log(response.status, response.config.url)
+}
